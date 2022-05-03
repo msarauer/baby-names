a52659b09edd04fd9486b1cb3f0521e54be9bf9f
Mitchel Sarauer <Mitchel.Sarauer@tpsgc-pwgsc.gc.ca>
2022-05-03 14:14:38 -0600

    Added get and post routes

---
+++ api/db/seeds/01_babies.sql
@@ -0,0 +1,5 @@
+INSERT INTO babies (name, slug)
+VALUES
+('finley', 'alk3SkdD'),
+('mitch', 'fewf3aDf'),
+('joe', 'pLkjJ9Js');
---
+++ api/postName/index.ts
@@ -0,0 +1,54 @@
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import * as pg from 'pg';
+
+const httpTrigger: AzureFunction = async function (
+	context: Context,
+	req: HttpRequest
+): Promise<void> {
+	context.log('HTTP trigger function processed a request.');
+	const config = {
+		host: process.env['PG_SERVER_NAME'],
+		user: process.env['PG_USERNAME'],
+		password: process.env['PG_PASSWORD'],
+		database: 'baby_names',
+		port: 5432,
+		ssl: true
+	};
+
+	const values = [req.body.name, req.body.slug];
+
+	const querySpec = {
+		text: 'INSERT INTO babies (name, slug) VALUES ($1, $2) RETURNING *',
+		values: values
+	};
+
+	console.log(querySpec.text);
+
+	try {
+		// Create a pool of connections
+		const pool = new pg.Pool(config);
+
+		// Get a new client connection from the pool
+		const client = await pool.connect();
+
+		// Execute the query against the client
+		const result = await client.query(querySpec);
+
+		// Release the connection
+		client.release();
+
+		// Return the query resuls back to the caller as JSON
+		context.res = {
+			status: 200,
+			isRaw: true,
+			body: result.rows,
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		};
+	} catch (err) {
+		context.log(err.message);
+	}
+};
+
+export default httpTrigger;
