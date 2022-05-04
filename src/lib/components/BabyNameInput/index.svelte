1ad0ed1029d924854ed8a1ca664f0ce4fa269f56
Mitchel Sarauer <Mitchel.Sarauer@tpsgc-pwgsc.gc.ca>
2022-05-04 15:50:14 -0600

    Added congrats message

--- src/lib/components/BabyNameInput/index.svelte
+++ src/lib/components/BabyNameInput/index.svelte
@@ -1,30 +1,46 @@
 <script lang="ts">
-	import { createAnswerKey } from '$lib/functions/createAnswerKey';
-	import { answerKey } from '../../stores/stores';
 	import { supabase } from '../../../supabase';
 
 	let babyName: string = '';
+	let submitted: boolean = false;
+	let url: string;
 
 	const handleSubmit = async () => {
 		let slug = (Math.random() + 1).toString(36).substring(7);
-		let name = babyName;
+		let name = babyName.toLowerCase();
 		const { data, error } = await supabase.from('babies').insert([{ name, slug }]);
 		if (error) {
 			return console.error(error);
 		}
-		console.log(data);
+		submitted = true;
+		url = `/${slug}`;
 	};
 </script>
 
 <form class="name-input" on:submit|preventDefault={handleSubmit}>
-	<input bind:value={babyName} />
-	<button type="submit">Submit</button>
+	{#if submitted}
+		<div class="congrats">
+			<p>Congrats on the new baby! Please share this link with your friends and family:</p>
+			<a href={url}>{window.location.origin}{url}</a>
+		</div>
+	{:else}
+		<input bind:value={babyName} />
+		<button type="submit">Submit</button>
+	{/if}
 </form>
 
 <style>
+	.congrats {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 	.name-input {
 		display: flex;
 		justify-content: center;
 		margin-bottom: 3em;
+	}
+	a {
+		color: white;
 	}
 </style>
