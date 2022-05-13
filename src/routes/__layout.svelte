23334254d8dfb06fdff6ef63ad64b282ba6a8843
Mitchel Sarauer <Mitchel.Sarauer@tpsgc-pwgsc.gc.ca>
2022-05-13 13:24:58 -0600

    Changed to border-box

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,10 +3,6 @@
 	import '../styles/app.css';
 </script>
 
-<svelte:head>
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-</svelte:head>
-
 <Header />
 <div class="container">
 	<slot />
